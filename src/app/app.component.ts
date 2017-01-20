f520efd816319a20b8c6d8a75ba7c76ee7431b02
Samuel Hindley López <samuelhindley@gmail.com>
2017-01-20 13:07:43 +0100

    English and spanish translation capability added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
+import { TranslateService } from 'ng2-translate/ng2-translate';
 
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
@@ -16,7 +17,7 @@ export class MyApp {
 
   pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform) {
+  constructor(public platform: Platform, public translate: TranslateService) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
@@ -25,6 +26,7 @@ export class MyApp {
       { title: 'Page Two', component: Page2 }
     ];
 
+    this.translateConfig();
   }
 
   initializeApp() {
@@ -40,5 +42,12 @@ export class MyApp {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
+  }
+
+  translateConfig() {
+    let userLang = navigator.language.split('-')[0];
+    userLang = /(en|es)/gi.test(userLang) ? userLang : 'es';
+    this.translate.setDefaultLang('en');
+    this.translate.use(userLang);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { NgModule, ErrorHandler } from '@angular/core';
+import { Http } from '@angular/http';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { TranslateModule, TranslateStaticLoader, TranslateLoader } from 'ng2-translate/ng2-translate';
 import { MyApp } from './app.component';
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
+
+export function createTranslateLoader(http: Http) {
+    return new TranslateStaticLoader(http, 'assets/i18n', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -11,7 +17,12 @@ import { Page2 } from '../pages/page2/page2';
     Page2
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    TranslateModule.forRoot({
+      provide: TranslateLoader,
+      useFactory: (createTranslateLoader),
+      deps: [Http]
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -21,4 +32,5 @@ import { Page2 } from '../pages/page2/page2';
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
+
 export class AppModule {}
