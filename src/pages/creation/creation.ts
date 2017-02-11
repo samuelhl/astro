976538bb71e7c6a02b9a5c493e9808dccdd10698
binkypv <missingno770@gmail.com>
2017-02-11 18:40:15 +0100

    Functionality added

--- src/pages/creation/creation.ts
+++ src/pages/creation/creation.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-
+import { ToastController } from 'ionic-angular';
 import { NavController } from 'ionic-angular';
+import { TranslateService } from 'ng2-translate/ng2-translate';
 
 @Component({
   selector: 'page-creation',
@@ -8,12 +9,63 @@ import { NavController } from 'ionic-angular';
 })
 export class CreationPage {
 
-  constructor(public navCtrl: NavController) {
+  public timeoutId: number;
 
+  private toastString: string;
+  private errorString: string;
+  private timeoutString: string;
+
+  places: {total: number, person: number} = {
+    total: 0,
+    person: 0
+  }
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public translateService: TranslateService) {
+    this.translateService.get('CREATED').subscribe((value) => {
+      this.toastString = value;
+    });
+
+    this.translateService.get('CREATION_ERROR').subscribe((value) => {
+      this.errorString = value;
+    });
+
+    this.translateService.get('TIMEOUT').subscribe((value) => {
+      this.timeoutString = value;
+    })
+
+    // Timeout (1 minute)
+    this.timeoutId = setTimeout(() => {
+      this.timeout();
+    }, 60000);
   }
 
   createObservation(){
+    if(this.places.person > this.places.total){
+      let toast = this.toastCtrl.create({
+        message: this.errorString,
+        duration: 3000,
+        position: 'top'
+      });
+      toast.present();
+    }else{
+      let toast = this.toastCtrl.create({
+        message: this.toastString,
+        duration: 3000,
+        position: 'top'
+      });
+      toast.present();
+      }
+  }
 
+  timeout() {
+    let toast = this.toastCtrl.create({
+          message: this.timeoutString,
+          duration: 2000,
+          position: 'top'
+        });
+    toast.present();
+    this.navCtrl.pop();
+    clearTimeout(this.timeoutId);
   }
 
 }
